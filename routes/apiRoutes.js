b7017e85a16679f65f5643cbcbae4cf8e02986a5
IronJon <lang.jeffrey@gmail.com>
2020-12-11 19:32:57 -0700

    There seems to be problems with index.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,16 @@ const dbNotes = require("../db/db.json");
 
 //Routing
 module.exports = (app) => {
-  //
+  //get request for when user visits page.
   app.get("/api/notes", (req, res) => res.json(dbNotes));
+  //post request for when user saves a note.
+  app.post("/api/notes", (req, res) => {
+    dbNotes.push(req.body);
+    console.log(req.body);
+  });
+  app.delete("/api/notes", (req, res) => {
+    let index = dbNotes.findIndex((item) => item.id === req.query.id);
+    dbNotes.splice(index, 1);
+    console.log(req.id);
+  });
 };
