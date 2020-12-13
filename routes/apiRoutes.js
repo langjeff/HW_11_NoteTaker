b4632f6505cc473871e1e1abefe8546dd2c61350
IronJon <lang.jeffrey@gmail.com>
2020-12-13 15:11:05 -0700

    app is working on db.json for all functionality. index.js still having issues.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,52 @@
 // file for data store and retrieval
 const dbNotes = require("../db/db.json");
+const cuid = require("cuid");
+const util = require("util");
+const fs = require("fs");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+//takes fs.readfile and allows you to use promises on it.
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 //Routing
 module.exports = (app) => {
   //get request for when user visits page.
-  app.get("/api/notes", (req, res) => res.json(dbNotes));
+  // in get route use readFileAsync, parse json, and then pass it.
+  app.get("/api/notes", (req, res) => {
+    res.json(dbNotes);
+    res.end();
+  });
   //post request for when user saves a note.
   app.post("/api/notes", (req, res) => {
-    dbNotes.push(req.body);
-    console.log(req.body);
+    let id = cuid();
+    let note = req.body;
+    note.id = id;
+
+    readFileAsync("./db/db.json", "utf-8").then((data) => {
+      let newData = JSON.parse(data);
+      // console.log(newData);
+      // take data and use spread operator
+      let updatedData = [...newData, note];
+      // console.log(updatedData);
+      //writefileAsync for updated
+      writeFileAsync("./db/db.json", JSON.stringify(updatedData));
+    });
+    res.json(dbNotes);
+    res.end();
   });
-  app.delete("/api/notes", (req, res) => {
-    let index = dbNotes.findIndex((item) => item.id === req.query.id);
-    dbNotes.splice(index, 1);
-    console.log(req.id);
+  // working on delete request, but it seems issues with index.js with buttons not working.
+  app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(req.params.id);
+    readFileAsync("./db/db.json", "utf-8").then((data) => {
+      let newData = JSON.parse(data);
+      let updatedData = newData.filter((note) => note.id !== id);
+      writeFileAsync("./db/db.json", JSON.stringify(updatedData));
+    });
+    res.json(dbNotes);
+    res.end();
   });
 };
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
-
 const express = require("express");
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
